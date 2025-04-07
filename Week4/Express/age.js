5d6fb31138e57ddf16c5a80838d19c6581443af4
itzAnshDahiya <anshdahiya00@gmail.com>
2025-04-07 23:53:36 +0530

    changes

--- Week4/Express/age.js
+++ Week4/Express/age.js
@@ -9,7 +9,9 @@ function isOldEnough(age) {
     return false;
   }
 }
-
+console.log(chalk.blue('I Am Always One Step Ahead'));
+console.log(chalk.blue('I Am Gona Rule This World'));
+console.log(chalk.blue('I Am Gona Become A Billionaire'));
 app.get("/ride1", function (req, res) {
   if (isOldEnough(req.query.age)) {
     res.json({
