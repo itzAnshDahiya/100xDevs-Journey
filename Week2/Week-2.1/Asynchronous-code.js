7eaca08bbd12a2f38f9b2b1da1d2c66c42287ce5
itzAnshDahiya <anshdahiya00@gmail.com>
2025-04-08 20:23:12 +0530

    Added week 5.1

--- Week2/Week-2.1/Asynchronous-code.js
+++ Week2/Week-2.1/Asynchronous-code.js
@@ -8,6 +8,9 @@ function print(err,data){
 }
 fs.readFile("aas.txt" , "utf-8" , print);
 console.log("Done!!");
+console.log(chalk.blue('I Am Always One Step Ahead'));
+console.log(chalk.blue('I Am Gona Rule This World'));
+console.log(chalk.blue('I Am Gona Become A Billionaire'));
 
 
 //Other example = timeout se 15 sec tk vo timeout chalta rahega background mai
